5fe0c63408325d1a5d22b2cf5ba07dda62b8b3f2
deltov <154072038+deltov@users.noreply.github.com>
2024-12-03 09:48:10 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,12 +1,12 @@
 from cryptography.fernet import Fernet
-from flask import Flask, rendertemplatestring, rendertemplate, jsonify
-from flask import rendertemplate
+from flask import Flask, render_template_string, render_template, jsonify
+from flask import render_template
 from flask import json
 from urllib.request import urlopen
 import sqlite3
-
-app = Flask(name)
-
+                                                                                                                                       
+app = Flask(__name__)                                                                                                                  
+                                                                                                                                       
 @app.route('/')
 def hello_world():
     return render_template('hello.html') 
@@ -19,7 +19,7 @@ def encryptage(valeur):
     valeur_bytes = valeur.encode()  # Conversion str -> bytes
     token = f.encrypt(valeur_bytes)  # Encrypt la valeur
     return f"Valeur encryptée : {token.decode()}"  # Retourne le token en str
-
+  
 @app.route('/decrypt/<string:token>')
 def decryptage(token):
     try:
@@ -28,6 +28,6 @@ def decryptage(token):
         return f"Valeur décryptée : {valeur.decode()}"  # Retourne la valeur en str
     except Exception as e:
         return f"Erreur de décryptage : {str(e)}"
-
-if __name == "__main":
+                                                                                                                                                     
+if __name__ == "__main__":
   app.run(debug=True)
